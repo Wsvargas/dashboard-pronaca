51949cea81ca7e2fb7dbdad765a9754e76ee8cc6
wsvargas <willianvargas1995@hotmail.com>
2026-02-24 16:54:55 -0500

    change sh

--- streamlit.sh
+++ streamlit.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-python3 -m streamlit run dashboard_produccion_ahora.py --server.port 8000 --server.address 0.0.0.0
+set -e
+
+cd /home/site/wwwroot
+
+# (Opcional) para que quede evidencia en Log Stream
+pwd
+ls -la .streamlit || true
+ls -la .streamlit/config.toml || true
+
+python3 -m streamlit run dashboard_produccion_ahora.py \
+  --server.port 8000 \
+  --server.address 0.0.0.0
